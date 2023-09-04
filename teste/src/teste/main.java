0c80e6f9d795f6c7bc89229048b6d307701cb878
renan-b-eth <4314751@alunouninter.com>
2023-09-04 11:56:50 -0300

    teste 10

--- teste/src/teste/main.java
+++ teste/src/teste/main.java
@@ -1,7 +1,21 @@
 package teste;
 
-public class main{
+import java.util.Scanner;
+
+import br.com.fiap.beans.Produto;
+
+public class main {
 	public static void main(String[] args) {
-		System.out.println("Hello World - TESTE");
+
+		Scanner in = new Scanner(System.in);
+		Produto p = new Produto();
+
+		p.setMarcaProduto(in.nextLine());
+		p.setQuantidadeProduto(in.nextInt());
+		p.setTipoProduto(in.nextLine());
+		p.setValorProduto(in.nextDouble());
+
+		System.out.println("Marca: " + p.getMarcaProduto() + "\n Quantidade: " + p.getMarcaProduto() + "\n Tipo: "
+				+ p.getTipoProduto() + "\n Valor: R$" + p.getValorProduto());
 	}
 }
